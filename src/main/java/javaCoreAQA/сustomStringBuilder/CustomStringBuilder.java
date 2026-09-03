51e1177afce79e7c10b496ffcd2776e009907eb0
Morican <burov-vitya@mail.ru>
2026-09-03 17:12:25 +0300

    Рефакторинг

--- src/main/java/javaCoreAQA/сustomStringBuilder/CustomStringBuilder.java
+++ src/main/java/javaCoreAQA/сustomStringBuilder/CustomStringBuilder.java
@@ -1,4 +1,4 @@
-package сustomStringBuilder;
+package javaCoreAQA.сustomStringBuilder;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
--- src/main/java/javaCoreAQA/сustomStringBuilder/Demo.java
+++ src/main/java/javaCoreAQA/сustomStringBuilder/Demo.java
@@ -1,4 +1,4 @@
-package сustomStringBuilder;
+package javaCoreAQA.сustomStringBuilder;
 
 public class Demo {
     public static void main(String[] args) {
